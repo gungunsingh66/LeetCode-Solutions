f7c9c40dca775efd7f7ee3f8dd3e1cd7d2190d3e
Gungun Singh <159872706+gungunsingh66@users.noreply.github.com>
2026-07-11 23:56:04 +0530

    Time: 16 ms (16.59%), Space: 45.6 MB (20.7%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    List<List<Integer>> answer = new ArrayList<>();
+    public void backtrack(int index, int remainingTarget, List<Integer> currentCombination ,int[] candidates){
+        Arrays.sort(candidates);
+        if(remainingTarget == 0){
+            answer.add(new ArrayList<>(currentCombination));
+            return;
+        }
+        if(remainingTarget < 0 || index> candidates.length){
+            return;
+        }
+        for(int i = index; i< candidates.length; i++){
+            if(i>index && candidates[i] == candidates[i-1]){
+                continue;
+            }
+            currentCombination.add(candidates[i]);
+            backtrack(i+1, remainingTarget - candidates[i], currentCombination, candidates);
+            currentCombination.remove(currentCombination.size() - 1);
+        }
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<Integer> currentCombination = new ArrayList<>();
+        backtrack(0, target , currentCombination, candidates);
+        return answer;
+    }
+}
