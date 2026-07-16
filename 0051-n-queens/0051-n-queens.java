44823e3a41964aab9472953f2bf7783c971d967d
Gungun Singh <159872706+gungunsingh66@users.noreply.github.com>
2026-07-16 16:00:01 +0530

    Time: 2 ms (82.99%), Space: 46.8 MB (28.58%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,48 @@
+class Solution {
+    List<List<String>> answer = new ArrayList<>();
+    public boolean isSafe(char[][] board,int row, int col, int n){
+        for(int i=row-1; i>=0 ; i--){
+            if(board[i][col]=='Q'){
+                    return false;
+                }
+        }
+        for(int i = row-1, j=col-1; i>=0 && j>=0 ; i--, j--){
+            if(board[i][j]=='Q' ){
+                    return false;
+                }
+        }
+        for(int i = row-1, j=col+1; i>=0  && j<n; i--, j++){ 
+            if(board[i][j]=='Q' ){
+                return false;
+            }
+        }
+        return true;
+    }
+    public void backtrack(char[][] board, int row , int n){
+        if(row == n){
+            List<String> solution = new ArrayList<>();
+            for(int i = 0; i<n; i++){
+                solution.add(new String(board[i]));
+            }
+            answer.add(solution);
+            return;
+        }
+        for(int col=0 ; col<n ; col++){
+            if(isSafe(board, row, col, n)){
+                board[row][col] = 'Q';
+                backtrack(board, row+1, n );
+                board[row][col] = '.';
+            }
+        }
+    }
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for(int i = 0 ; i< n ; i++){
+            for(int j = 0 ; j<n; j++){
+                board[i][j] = '.';
+            }
+        }
+        backtrack(board , 0, n);
+        return answer;
+    }
+}
