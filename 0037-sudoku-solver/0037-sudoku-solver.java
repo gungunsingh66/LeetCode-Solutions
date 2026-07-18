b05466fd4e6f71cd900bb04640e4deda4d3bdccd
Gungun Singh <159872706+gungunsingh66@users.noreply.github.com>
2026-07-18 12:19:01 +0530

    Time: 215 ms (67.72%), Space: 43 MB (17.37%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,18 +1,10 @@
 class Solution {
     public boolean isValid(char[][] board, int row , int col,char ch){
-        //row check
         for(int i = 0 ; i<9; i++){
-            if(board[row][i]== ch){
+            if(board[row][i]== ch || board[i][col] == ch){
                 return false;
             }
         }
-        //col check
-        for(int j = 0 ; j<9 ; j++){
-            if(board[j][col] == ch){
-                return false;
-            }
-        }
-        //subgrid check
         int startrow = (row/3) * 3;
         int startcol = (col/3) * 3;
         for(int i = startrow; i<startrow+3; i++){
@@ -45,6 +37,8 @@ class Solution {
         return true;
     }
     public void solveSudoku(char[][] board) {
-        backtrack(board);
+        if(board != null){
+            backtrack(board);
+        }
     }
 }
