11b09bd29b81f9d717ec57c0a05981a08014ac52
Gungun Singh <159872706+gungunsingh66@users.noreply.github.com>
2026-07-28 17:49:25 +0530

    Time: 0 ms (100%), Space: 43.4 MB (93.7%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,27 +14,42 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i = 0; i< size; i++){
-                TreeNode current = q.poll();
-                if(i == size-1){
-                    ans.add(current.val);
-                }
-
-                if(current.left != null){
-                    q.offer(current.left);
-                }
-                if(current.right != null){
-                    q.offer(current.right);
-                }
-            }
+    private void dfs(TreeNode root, int depth , List<Integer> ans){
+        if(root == null) return;
+        if(ans.size() == depth){
+            ans.add(root.val);
         }
+        dfs(root.right, depth+1, ans);
+        dfs(root.left, depth+1, ans);
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        dfs(root, 0 , ans);
         return ans;
+
+
+
+
+        // Queue<TreeNode> q = new LinkedList<>();
+        // List<Integer> ans = new ArrayList<>();
+        // if(root == null) return ans;
+        // q.offer(root);
+        // while(!q.isEmpty()){
+        //     int size = q.size();
+        //     for(int i = 0; i< size; i++){
+        //         TreeNode current = q.poll();
+        //         if(i == size-1){
+        //             ans.add(current.val);
+        //         }
+
+        //         if(current.left != null){
+        //             q.offer(current.left);
+        //         }
+        //         if(current.right != null){
+        //             q.offer(current.right);
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 }
