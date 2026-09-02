9d367f4b8fe81b27119e972bb609390b51368d4a
Gungun Singh <159872706+gungunsingh66@users.noreply.github.com>
2026-09-02 21:51:38 +0530

    Time: 1 ms (31.89%), Space: 45.5 MB (5.31%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,5 +1,29 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+        //return true;
+
+        boolean hasOdd = false;
+        boolean hasEven = false;
+
+        for (int num : nums1) {
+
+            if (num % 2 == 0) {
+                hasEven = true;
+            } else {
+                hasOdd = true;
+            }
+        }
+
+        // If all numbers already have the same parity
+        if (!hasOdd || !hasEven) {
+            return true;
+        }
+
+        // Mixed parity:
+        // Keep any odd number unchanged.
+        // For every even number, subtract an odd number:
+        // even - odd = odd.
         return true;
+
     }
 }
