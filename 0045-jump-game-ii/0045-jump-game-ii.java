a32958d4a3665179de78c2474f282af21916f6cf
Gungun Singh <159872706+gungunsingh66@users.noreply.github.com>
2026-09-13 00:33:14 +0530

    Time: 1 ms (99.78%), Space: 47.4 MB (38.69%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+        for(int i = 0; i < n-1; i++){
+            farthest = Math.max(farthest, nums[i] + i);
+            if(i == currentEnd){
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+        return jumps;
+    }
+}
